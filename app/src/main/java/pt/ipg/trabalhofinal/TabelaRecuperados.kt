01d9865f61ce9196325b4dcd4dd9fe17a6f6f564
Rafael Santos <rdsrafael@gmail.com>
2021-05-21 16:41:57 +0100

    Criação de constantes para os campos das tabelas pessoas e ativos

---
+++ app/src/main/java/pt/ipg/trabalhofinal/TabelaRecuperados.kt
@@ -0,0 +1,9 @@
+package pt.ipg.trabalhofinal
+
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaRecuperados {
+}
