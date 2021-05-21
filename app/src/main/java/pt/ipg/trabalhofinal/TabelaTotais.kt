64565697614e8e175209df10e955740692885523
Rafael Santos <rdsrafael@gmail.com>
2021-05-21 16:48:10 +0100

    Criação de constantes para os campos das tabela totais

--- app/src/main/java/pt/ipg/trabalhofinal/TabelaTotais.kt
+++ app/src/main/java/pt/ipg/trabalhofinal/TabelaTotais.kt
@@ -5,5 +5,37 @@ import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.provider.BaseColumns
 
-class TabelaTotais {
+class TabelaTotais (db: SQLiteDatabase) : BaseColumns {
+    private val db : SQLiteDatabase = db
+    fun cria() = db?.execSQL("CREATE TABLE $NOME_TABELA (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT)")
+
+    fun insert(values: ContentValues): Long {
+        return db.insert(NOME_TABELA, null, values)
+    }
+
+    fun update(values: ContentValues, whereClause: String, whereArgs: Array<String>): Int {
+        return db.update(NOME_TABELA, values, whereClause, whereArgs)
+    }
+
+    fun delete(whereClause: String, whereArgs: Array<String>): Int {
+        return db.delete(NOME_TABELA, whereClause, whereArgs)
+    }
+
+    fun query(
+        columns: Array<String>,
+        selection: String,
+        selectionArgs: Array<String>,
+        groupBy: String, having: String,
+        orderBy: String
+    ): Cursor? {
+        return db.query(NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy)
+    }
+
+    companion object{
+        const val NOME_TABELA = "CasosTotais"
+        const val CAMPO_TOTAIS = "NTotais"
+        const val CAMPO_ATIVOS = "NAtivos"
+        const val CAMPO_RECUPERADOS = "NRecuperados"
+    }
+
 }
