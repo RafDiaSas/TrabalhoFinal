f52757d325463c12aeeb0680aa6fcfecdd59b6fa
Rafael Santos <rdsrafael@gmail.com>
2021-06-04 18:50:12 +0100

    Teste para ler Recuperados

--- app/src/androidTest/java/pt/ipg/trabalhofinal/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/pt/ipg/trabalhofinal/ExampleInstrumentedTest.kt
@@ -324,7 +324,7 @@ class TesteBaseDados {
     fun consegueApagarRecuperados(){
         val db = getBDCasosOpenHelper().writableDatabase
         val tabelaPessoas = getTabelaPessoas(db)
-        val pessoas = Pessoas(nome="Daniel", numeroCC = "87690271", telefone = "+355 939128700", estado = 1, dataD = 25102020, dataR = 31122020)
+        val pessoas = Pessoas(nome="Daniel", numeroCC = "87690271", telefone = "+355 939128700", estado = 2, dataD = 25102020, dataR = 31122020)
 
 
         pessoas.id = inserePessoas(tabelaPessoas, pessoas)
@@ -339,6 +339,24 @@ class TesteBaseDados {
         )
 
         assertEquals(1, registosEliminados)
+
+        db.close()
+    }
+
+    @Test
+    fun consegueLerRecuperados(){
+        val db = getBDCasosOpenHelper().writableDatabase
+        val tabelaPessoas = getTabelaPessoas(db)
+        val pessoas = Pessoas(nome="Daniel", numeroCC = "87690271", telefone = "+355 939128700", estado = 2, dataD = 25102020, dataR = 31122020)
+        pessoas.id = inserePessoas(tabelaPessoas, pessoas)
+
+        val tabelaRecuperados = getTabelaRecuperados(db)
+        val recuperados = Recuperados(idPessoas = pessoas.id)
+        recuperados.id = insereRecuperados(tabelaRecuperados, recuperados)
+
+        val recuperadosBD = getRecuperadosBD(tabelaRecuperados, pessoas.id)
+        assertEquals(pessoas, recuperadosBD)
+
 
         db.close()
     }
