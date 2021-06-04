dbce984d601251f28d60581a4d4ecc04e01c3cec
Rafael Santos <rdsrafael@gmail.com>
2021-06-04 16:41:28 +0100

    Criada classe Ativos

---
+++ app/src/main/java/pt/ipg/trabalhofinal/Ativos.kt
@@ -0,0 +1,26 @@
+package pt.ipg.trabalhofinal
+
+import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
+
+data class Ativos(var id: Long = -1, var idPessoas: Int) {
+    fun toContentValues(): ContentValues {
+        val valores = ContentValues().apply{
+            put(TabelaAtivos.CAMPO_ID_PESSOAS, idPessoas)
+        }
+        return valores
+    }
+
+    companion object {
+        fun fromCursor(cursor: Cursor): Ativos {
+            val colId = cursor.getColumnIndex(BaseColumns._ID)
+            val colIdPessoas = cursor.getColumnIndex(TabelaAtivos.CAMPO_ID_PESSOAS)
+
+            val id = cursor.getLong(colId)
+            val idPessoas = cursor.getInt(colIdPessoas)
+
+            return Ativos(id, idPessoas)
+        }
+    }
+}
