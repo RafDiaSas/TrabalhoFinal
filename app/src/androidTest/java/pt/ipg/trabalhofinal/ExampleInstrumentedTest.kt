0ac48e3301e57e454aa01aa3e519eeb1020255da
Rafael Santos <rdsrafael@gmail.com>
2021-06-04 18:37:21 +0100

    Teste para ler Ativos

--- app/src/androidTest/java/pt/ipg/trabalhofinal/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/pt/ipg/trabalhofinal/ExampleInstrumentedTest.kt
@@ -255,5 +255,21 @@ class TesteBaseDados {
         db.close()
     }
 
+    @Test
+    fun consegueLerAtivos(){
+        val db = getBDCasosOpenHelper().writableDatabase
+        val tabelaPessoas = getTabelaPessoas(db)
+        val pessoas = Pessoas(nome="Daniel", numeroCC = "87690271", telefone = "+355 939128700", estado = 1, dataD = 25102020, dataR = 31122020)
+        pessoas.id = inserePessoas(tabelaPessoas, pessoas)
 
+        val tabelaAtivos = getTabelaAtivos(db)
+        val ativos = Ativos(idPessoas = pessoas.id)
+        ativos.id = insereAtivos(tabelaAtivos, ativos)
+
+        val ativosBD = getAtivosBD(tabelaAtivos, pessoas.id)
+        assertEquals(pessoas, ativosBD)
+
+
+        db.close()
+    }
 }
