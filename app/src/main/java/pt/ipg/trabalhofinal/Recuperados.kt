80906f3ccd0003f89c9d2130fb4899e1e0aed7c7
Rafael Santos <rdsrafael@gmail.com>
2021-06-04 16:44:45 +0100

    Criada classe Recuperados

---
+++ app/src/main/java/pt/ipg/trabalhofinal/Recuperados.kt
@@ -0,0 +1,26 @@
+package pt.ipg.trabalhofinal
+
+import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
+
+data class Recuperados(var id: Long = -1, var idPessoas: Int) {
+    fun toContentValues(): ContentValues {
+        val valores = ContentValues().apply{
+            put(TabelaRecuperados.CAMPO_ID_PESSOAS, idPessoas)
+        }
+        return valores
+    }
+
+    companion object {
+        fun fromCursor(cursor: Cursor): Recuperados {
+            val colId = cursor.getColumnIndex(BaseColumns._ID)
+            val colIdPessoas = cursor.getColumnIndex(TabelaRecuperados.CAMPO_ID_PESSOAS)
+
+            val id = cursor.getLong(colId)
+            val idPessoas = cursor.getInt(colIdPessoas)
+
+            return Recuperados(id, idPessoas)
+        }
+    }
+}
